5131955c8b5683f6d9439560e3f69a9161bed724
CDiard <91243460+CDiard@users.noreply.github.com>
2023-05-17 09:41:28 +0200

    Liste marvel : Page suivante

--- src/Component/MarvelComponent/MarvelComponent.jsx
+++ src/Component/MarvelComponent/MarvelComponent.jsx
@@ -3,21 +3,44 @@ import React, {useEffect, useState} from 'react';
 const MarvelComponent = () => {
     const [items, setItems] = useState([]);
     const [isLoading, setLoading] = useState(false);
+    const [page, setPage] = useState(1);
 
     useEffect(() => {
+        setLoading(false)
+
         fetch('http://127.0.0.1:8000/api/marvels', {
+            method: 'GET',
             headers: {
                 "Content-Type": "application/json",
                 "Authorization": "Bearer "+localStorage.getItem("tokenUser")
-            },
-            method: 'GET',
+            }
         }).then(response => response.json())
             .then(data => {
                 setItems(data)
                 setLoading(true)
+                setPage(1)
             })
             .catch(error => console.error(error));
     }, []);
+
+    const handleClick = () => {
+        if (isLoading === true) {
+            setLoading(false)
+            fetch('http://127.0.0.1:8000/api/marvels?page='+(page+1), {
+                method: 'GET',
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": "Bearer "+localStorage.getItem("tokenUser")
+                }
+            }).then(response => response.json())
+                .then(data => {
+                    setItems(data)
+                    setPage(page+1)
+                    setLoading(true)
+                })
+                .catch(error => console.error(error));
+        }
+    };
 
     return (
         <>
@@ -44,6 +67,9 @@ const MarvelComponent = () => {
                             <p>Chargement des données</p>
                         </div>
                     )}
+                </div>
+                <div className="boutons">
+                    {isLoading === true && page < 9 ? (<button onClick={handleClick}>Voir plus</button>) : ''}
                 </div>
             </div>
         </>
